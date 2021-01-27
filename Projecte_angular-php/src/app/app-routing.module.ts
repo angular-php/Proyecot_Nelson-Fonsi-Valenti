ffe7603deebf9b40f1c667a5415b9cb229e880cc
MarcB98 <marcbatalla652@gmail.com>
2021-01-27 17:17:37 +0100

    1r commit Registro

--- Projecte_angular-php/src/app/app-routing.module.ts
+++ Projecte_angular-php/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistroComponent } from './components/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", pathMatch: "full", redirectTo:"registro"},
+  {path: 'registro', component: RegistroComponent},
+  {path: "**" , redirectTo:"registro"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
