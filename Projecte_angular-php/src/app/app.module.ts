5b42ee538dd21601146358ff768d347f8e5db707
MarcB98 <marcbatalla652@gmail.com>
2021-01-29 21:28:18 +0100

    Validaciones Registro (No terminado)

--- Projecte_angular-php/src/app/app.module.ts
+++ Projecte_angular-php/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistroComponent } from './components/registro/registro.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { RegistroComponent } from './components/registro/registro.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Projecte_angular-php/src/app/components/registro/registro.component.ts
+++ Projecte_angular-php/src/app/components/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { RxwebValidators } from '@rxweb/reactive-form-validators';
 
 @Component({
   selector: 'app-registro',
@@ -8,6 +10,18 @@ import { Component, OnInit } from '@angular/core';
 export class RegistroComponent implements OnInit {
 
   constructor() { }
+
+  registro = new FormGroup({
+
+    nick : new FormControl('' , [Validators.required,Validators.minLength(4),Validators.maxLength(15)]),
+    pass : new FormControl('' , [Validators.required,Validators.minLength(4),Validators.maxLength(15)]),
+    cpass : new FormControl('' , [Validators.required, RxwebValidators.compare({fieldName:'pass'})]),
+    email : new FormControl('' , Validators.required),
+    firstname : new FormControl('' , [Validators.required,Validators.minLength(3)]),
+    lastname : new FormControl('' , [Validators.required,Validators.minLength(3)]),
+    centro : new FormControl('' , Validators.required)
+
+  });
 
   ngOnInit(): void {
   }
