c681314cf2c1189a7be0b57fcabc81b24b172c3d
MarcB98 <marcbatalla652@gmail.com>
2021-01-30 10:29:49 +0100

    Validaciones Registro (No terminado)
    Sweet Alerts puestos

--- Projecte_angular-php/src/app/components/registro/registro.component.ts
+++ Projecte_angular-php/src/app/components/registro/registro.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { RxwebValidators } from '@rxweb/reactive-form-validators';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registro',
@@ -16,12 +17,35 @@ export class RegistroComponent implements OnInit {
     nick : new FormControl('' , [Validators.required,Validators.minLength(4),Validators.maxLength(15)]),
     pass : new FormControl('' , [Validators.required,Validators.minLength(4),Validators.maxLength(15)]),
     cpass : new FormControl('' , [Validators.required, RxwebValidators.compare({fieldName:'pass'})]),
-    email : new FormControl('' , Validators.required),
+    email : new FormControl('' , [Validators.required,Validators.email]),
     firstname : new FormControl('' , [Validators.required,Validators.minLength(3)]),
     lastname : new FormControl('' , [Validators.required,Validators.minLength(3)]),
     centro : new FormControl('' , Validators.required)
 
   });
+
+  sweetAlertRegistrarte() {
+    Swal.fire({
+      icon: 'success',
+      text: 'Usuaro registrado!',
+    })
+  }
+
+  sweetAlertLogin() {
+    Swal.fire({
+      title: 'Estas seguro que tienes cuenta?',
+      showDenyButton: true,
+      confirmButtonText: `Si`,
+      denyButtonText: `No`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire('Perfecto!', 'Vamos al registro....', 'success')
+      } else if (result.isDenied) {
+        Swal.fire('Registrate primero!', '', 'info')
+      }
+    })
+  }
 
   ngOnInit(): void {
   }
