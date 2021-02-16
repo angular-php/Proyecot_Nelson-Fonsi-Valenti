f09ca20eb6ca286d2b6b49c8b67161cf366eeb8e
quimmp26 <quiim2000@gmail.com>
2021-02-16 18:43:44 +0100

    perfil ALUMNO y PROFE

--- Projecte_angular-php/src/app/components/perfil/perfil.component.ts
+++ Projecte_angular-php/src/app/components/perfil/perfil.component.ts
@@ -3,6 +3,7 @@ import { Usuario } from 'src/app/models/usuario.model';
 import { Ranking } from 'src/app/models/ranking.model';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { UsuarioService } from 'src/app/services/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-perfil',
@@ -10,11 +11,15 @@ import { UsuarioService } from 'src/app/services/usuario.service';
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
+
   nickname: string;
   usuario: Usuario;
   rankingArray: Ranking[] = [];
+  student: boolean = false;
+  hideCenter: boolean = true;
 
   perfilForm: FormGroup;
+
 
 
   validation_messages = {
@@ -35,6 +40,10 @@ export class PerfilComponent implements OnInit {
       { type: 'required', message: 'El campo contraseña es obligatorio' },
       { type: 'minlength', message: 'El campo contraseña debe contener como mínimo 6 carácteres' },
     ],
+    center: [
+      { type: 'required', message: 'El campo centro es obligatorio' },
+      { type: 'minlength', message: 'El campo centro debe contener como mínimo 5 carácteres' },
+    ],
   };
 
 
@@ -46,6 +55,7 @@ export class PerfilComponent implements OnInit {
       lname: new FormControl('', [Validators.required, Validators.minLength(3)]),
       email: new FormControl('', [Validators.required, Validators.minLength(5), Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      center: new FormControl('')
     });
 
   }
@@ -59,21 +69,92 @@ export class PerfilComponent implements OnInit {
   }
 
   selectUser(id) {
-    this.usuarioService.getUsuario(id).subscribe((resp => {
-      this.usuario = new Usuario(resp[0].nick, resp[0].firstname, resp[0].lastname, resp[0].email, resp[0].password, this.rankingArray);
-      console.log(this.usuario);
-      this.nickname = resp[0].nick;
-      this.perfilForm.setValue({
-        fname: this.usuario.firstname,
-        lname: this.usuario.lastname,
-        email: this.usuario.email,
-        password: this.usuario.password
-      });
+    if(this.student == true){
+      this.hideCenter = true;
+      this.usuarioService.getAlumno(id).subscribe((resp => {
+        this.usuario = new Usuario(resp[0].id ,resp[0].nick, resp[0].firstname, resp[0].lastname, resp[0].email, resp[0].password, this.rankingArray);
+        console.log(this.usuario);
+        this.nickname = resp[0].nick;
+        this.perfilForm.setValue({
+          fname: this.usuario.firstname,
+          lname: this.usuario.lastname,
+          email: this.usuario.email,
+          password: this.usuario.password,
+          center: null
+        });
+
+      }), (e => {
+        console.log(e);
+      }));
+    }else{
+      this.hideCenter = false;
+      this.perfilForm.controls["center"].setValidators([Validators.required,
+        Validators.minLength(5)]);
+      this.perfilForm.controls['center'].updateValueAndValidity();
+
+      this.usuarioService.getProfesor(id).subscribe((resp => {
+        this.usuario = new Usuario(resp[0].id ,resp[0].nick, resp[0].firstname, resp[0].lastname, resp[0].email, resp[0].password, this.rankingArray, null, resp[0].centro);
+        console.log(this.usuario);
+        this.nickname = resp[0].nick;
+        this.perfilForm.setValue({
+          fname: this.usuario.firstname,
+          lname: this.usuario.lastname,
+          email: this.usuario.email,
+          password: this.usuario.password,
+          center: this.usuario.center
+        });
+
+      }), (e => {
+        console.log(e);
+      }));
+    }
+
+  }
 
 
-    }), (e => {
-      console.log(e);
-    }));
+
+  saveUser() {
+    if(this.student == true) {
+      this.usuario = new Usuario(1, this.nickname, this.perfilForm.controls.fname.value, this.perfilForm.controls.lname.value, this.perfilForm.controls.email.value, this.perfilForm.controls.password.value, this.rankingArray);
+      this.usuarioService.updateAlumno(this.usuario).subscribe((resp => {
+        console.log(resp);
+
+      }), (e => {
+        console.log(e);
+      }));
+    }else{
+      this.usuario = new Usuario(1, this.nickname, this.perfilForm.controls.fname.value, this.perfilForm.controls.lname.value, this.perfilForm.controls.email.value, this.perfilForm.controls.password.value, this.rankingArray, null, this.perfilForm.controls.center.value);
+      this.usuarioService.updateProfesor(this.usuario).subscribe((resp => {
+        console.log(resp);
+        if(resp['resultado'] == 'OK'){
+          Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Bien!',
+            text: resp['mensaje'],
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }else {
+          Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: 'Ups... algo ha ido mal',
+            text: "Error al guardar los datos!",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      }), (e => {
+        console.log(e);
+      }));
+    }
+
+  }
+
+  savePassword() {
+    const pass = this.perfilForm.controls.password.value;
+    console.log(pass);
   }
 
 }
--- Projecte_angular-php/src/app/services/usuario.service.ts
+++ Projecte_angular-php/src/app/services/usuario.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Usuario } from '../models/usuario.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,19 @@ export class UsuarioService {
 
   constructor(private http: HttpClient) { }
 
-  getUsuario(id: number) {
-    return this.http.get(`${this.URL}seleccionarUsuarios.php?id=${id}`);
+  getAlumno(id: number) {
+    return this.http.get(`${this.URL}seleccionarAlumno.php?id=${id}`);
+  }
+
+  getProfesor(id: number) {
+    return this.http.get(`${this.URL}seleccionarProfesor.php?id=${id}`);
+  }
+
+  updateAlumno(user: Usuario) {
+    return this.http.post(`${this.URL}modificarAlumno.php`, JSON.stringify(user));
+  }
+
+  updateProfesor(user: Usuario) {
+    return this.http.post(`${this.URL}modificarProfesor.php`, JSON.stringify(user));
   }
 }
