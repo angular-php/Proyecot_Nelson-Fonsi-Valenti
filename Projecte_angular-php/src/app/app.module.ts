56fdacd1fa3343aad52348b3907103913a457471
Marc Batalla Tapia <57674196+MarcB98@users.noreply.github.com>
2021-05-04 17:20:39 +0200

    Fase 4

--- Projecte_angular-php/src/app/app.module.ts
+++ Projecte_angular-php/src/app/app.module.ts
@@ -12,6 +12,9 @@ import { VistaRankingComponent } from './components/vista-ranking/vista-ranking.
 import { ModificarRankingComponent } from './components/modificar-ranking/modificar-ranking.component';
 import { FormRankingAlumnoComponent } from './components/form-ranking-alumno/form-ranking-alumno.component';
 
+import { EvaluarSkillsComponent } from './components/evaluar-skills/evaluar-skills.component';
+import { HistorialComponent } from './components/historial/historial.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,6 +25,8 @@ import { FormRankingAlumnoComponent } from './components/form-ranking-alumno/for
     FormRankingComponent,
     ModificarRankingComponent,
     FormRankingAlumnoComponent,
+    EvaluarSkillsComponent,
+    HistorialComponent,
   ],
   imports: [
     BrowserModule,
