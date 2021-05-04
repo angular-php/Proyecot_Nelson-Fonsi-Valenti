f6f1fb8297303642da4a78a13b5eb07dcd663adf
Marc Batalla Tapia <57674196+MarcB98@users.noreply.github.com>
2021-05-04 16:13:31 +0200

    Imatges SKILLS amb PNG

--- Projecte_angular-php/src/environments/environment.ts
+++ Projecte_angular-php/src/environments/environment.ts
@@ -5,10 +5,9 @@
 export const environment = {
   production: false,
 
+  // url: "http://localhost:5500/Proyecto_Nelson-Fonsi-Valenti/Projecte_angular-php/"
 
-  url: "http://localhost:5500/Proyecto_Nelson-Fonsi-Valenti/Projecte_angular-php/"
-
-  // url: "http://localhost:8080/"
+  url: "http://localhost:8080/"
 
 };
 
